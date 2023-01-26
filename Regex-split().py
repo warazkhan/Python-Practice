b3e755742bf14950f24cd20c14e966d90b3ef931
Wara Zubair Khan <113543605+warazkhan@users.noreply.github.com>
2023-01-26 15:54:42 +0100

    Create Regex-split().py

---
+++ Regex-split().py
@@ -0,0 +1,8 @@
+#You are given a string  consisting only of digits 0-9, commas ,, and dots .
+#Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in .
+#It’s guaranteed that every comma and every dot in  is preceeded and followed by a digit.
+
+regex_pattern = r"[,.]"
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
